9b5392989743451e524b0b665d69e6bf5a129aa0
matteobregola <matteobregola@gmail.com>
2023-12-03 15:08:36 +0100

    still a draft

--- src/graph.rs
+++ src/graph.rs
@@ -35,8 +35,16 @@ fn into_graph(robot_map: Option<Vec<Vec<Option<Tile>>>>) -> Graph<(usize, usize)
                     match vec[i][j].as_ref() {
                         None => {}
                         Some(present_tile) => {
-                            // this also checks that the current tile is walkable (because the robot
-                            // visited it)
+                            // this checks if the robot walked over the tile or if he has
+                            // seen it.
+
+                            if !present_tile.tile_type.properties().walk(){
+                                // Since the vector contains also Tiles that the robot has seen
+                                // we have to check if the tile we are looking at is walkable or not
+                                // if not i don't need it in the graph
+                                continue;
+                            }
+
                             let current_node=graph.add_node((i, j));
 
                             if present_tile.tile_type == TileType::Teleport(true){
@@ -49,11 +57,18 @@ fn into_graph(robot_map: Option<Vec<Vec<Option<Tile>>>>) -> Graph<(usize, usize)
                                 match vec[i][j+1].as_ref(){
                                     None => {}
                                     Some(next_tile) => {
-                                        // again if is Some then is walkable so i can add this node
-                                        // and create the edge
+                                        // this checks if the robot walked over the tile or if he has
+                                        // seen it.
+                                        if !present_tile.tile_type.properties().walk(){
+                                            // Since the vector contains also Tiles that the robot has seen
+                                            // we have to check if the tile we are looking at is walkable or not
+                                            // if not i don't need it in the graph
+                                            continue;
+                                        }
+
                                         let next_node=graph.add_node((i,j+1));
                                         graph.add_edge(current_node,next_node,
-                                                       eval_weight((i,j),(i,j+1)))
+                                                       eval_weight((i,j),(i,j+1)));
                                     }
                                 }
                             }
@@ -67,7 +82,7 @@ fn into_graph(robot_map: Option<Vec<Vec<Option<Tile>>>>) -> Graph<(usize, usize)
                                         // and create the edge
                                         let next_node=graph.add_node((i+1,j));
                                         graph.add_edge(current_node,next_node,
-                                                       eval_weight((i,j),(i+1,j)))
+                                                       eval_weight((i,j),(i+1,j)));
                                     }
                                 }
                             }
@@ -81,7 +96,7 @@ fn into_graph(robot_map: Option<Vec<Vec<Option<Tile>>>>) -> Graph<(usize, usize)
     for (index,current_teleport) in teleports.iter().enumerate(){
         for i in index+1..teleports.len()-1{
             let c1=graph.node_weight(*current_teleport).unwrap();
-            let c2=graph.node_weight(*teleports[i+1]).unwrap();
+            let c2=graph.node_weight(teleports[i+1]).unwrap();
             graph.add_edge(*current_teleport,teleports[i+1], eval_weight(*c1,*c2));
         }
     }
@@ -126,4 +141,66 @@ fn test_library_usage(){
 
     println!("Il costo da (0,0) a (2,1) e': {:?}", cost); //3 step perchè non posso andare in diagonale.
 
+}
+
+macro_rules! set_tile_type {
+    ($map:expr, $row:expr, $col:expr, $tile_type:expr) => {
+        if let Some(row) = $map.get_mut($row) {
+            if let Some(tile_option) = row.get_mut($col) {
+                if let Some(tile) = tile_option.as_mut() {
+                    tile.tile_type = $tile_type;
+                }
+            }
+        }
+    };
+}
+
+fn find_node_by_data(graph: &UnGraph<(usize,usize), u32>, target_data: (usize,usize)) -> Option<petgraph::graph::NodeIndex> {
+    graph.node_indices().find(|&node| graph[node] == target_data)
+}
+
+
+#[test]
+fn test_into_graph_v1(){
+    // see example at: ./../docfiles/world_example.png
+    let mut walkable=Tile{
+        tile_type: TileType::Sand,
+        content: Content::Rock(1),
+        elevation: 0,
+    };
+
+    let not_walkable=Tile{
+        tile_type: TileType::DeepWater,
+        content: Content::Coin(1),
+        elevation: 10,
+    };
+
+    let mut robot_map= Vec::new();
+
+    for i in 0..5{
+        let mut row_vector= Vec::new();
+        for i in 0..5{
+            if i==2 || i==4{
+                row_vector.push(Some(not_walkable.clone())); //t10 -->t14
+            }
+            else{
+                row_vector.push(Some(walkable.clone()));
+            }
+        }
+        robot_map.push(row_vector);
+    }
+
+    set_tile_type!(robot_map, 0, 3, TileType::DeepWater); //t3
+    set_tile_type!(robot_map, 0, 4, TileType::Teleport(true)); // t4
+    set_tile_type!(robot_map, 1, 1, TileType::DeepWater); //t6
+    set_tile_type!(robot_map, 1, 2, TileType::Teleport(true)); //t7
+    set_tile_type!(robot_map, 3, 4, TileType::Teleport(true)); //t18
+
+    let graph=into_graph(Some(robot_map));
+
+    let start_node=find_node_by_data(&graph, (0,0)).unwrap();
+    let target_node = find_node_by_data(&graph, (0,4)).unwrap();
+    let result= dijkstra(&graph, start_node, None, |e| *e.weight());
+    let cost=result.get(&target_node).unwrap_or(&1000);
+    println!("Il costo da (0,0) a (0,4) e': {:?}", cost);
 }
