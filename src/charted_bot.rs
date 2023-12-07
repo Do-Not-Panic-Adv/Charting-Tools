a615a6e09bd51745c851d3f12eddcae3a81667b2
thegavereguy <dallacia.enrico@protonmail.com>
2023-12-07 17:31:23 +0100

    Stuff non ho voglia

--- src/charted_bot.rs
+++ src/charted_bot.rs
@@ -1,30 +1,32 @@
 use robotics_lib::{
+    energy::Energy,
     interface::{discover_tiles, Direction},
     runner::Runnable,
     world::World,
 };
 
-use crate::{
-    charted_coordinate::ChartedCoordinate,
-    charted_map::{ChartedMap, MapKey},
-};
+use crate::charted_coordinate::ChartedCoordinate;
 
 pub struct ChartedBot {
-    energy: u32,
+    energy: Energy,
+    coordinates: ChartedCoordinate,
 }
 
 impl ChartedBot {
-    pub fn new(energy: u32) -> Self {
-        ChartedBot { energy }
+    pub fn new(energy: Energy, coordinates: ChartedCoordinate) -> Self {
+        ChartedBot {
+            energy,
+            coordinates,
+        }
     }
 
-    fn discover(
+    pub fn discover(
         &mut self,
         robot: &mut impl Runnable,
         world: &mut World,
         start_pos: ChartedCoordinate,
         dir: Direction,
     ) {
-        discover_tiles(robot, world, &vec![(0usize, 1usize)]);
+        //let res = discover_tiles(robot, world, &vec![(0usize, 1usize)]);
     }
 }
