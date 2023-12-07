49981dd0d5e44fd63c4552fce6eb3d6a71ccff3c
thegavereguy <dallacia.enrico@protonmail.com>
2023-12-07 01:01:09 +0100

    simple stuff

--- src/charted_bot.rs
+++ src/charted_bot.rs
@@ -1,3 +1,30 @@
-fn test() {
-    println!("testbot")
+use robotics_lib::{
+    interface::{discover_tiles, Direction},
+    runner::Runnable,
+    world::World,
+};
+
+use crate::{
+    charted_coordinate::ChartedCoordinate,
+    charted_map::{ChartedMap, MapKey},
+};
+
+pub struct ChartedBot {
+    energy: u32,
+}
+
+impl ChartedBot {
+    pub fn new(energy: u32) -> Self {
+        ChartedBot { energy }
+    }
+
+    fn discover(
+        &mut self,
+        robot: &mut impl Runnable,
+        world: &mut World,
+        start_pos: ChartedCoordinate,
+        dir: Direction,
+    ) {
+        discover_tiles(robot, world, &vec![(0usize, 1usize)]);
+    }
 }
