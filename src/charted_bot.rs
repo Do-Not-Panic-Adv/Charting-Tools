a68eacbb5f64d0eafa78a624d20dcc5daf4d671c
thegavereguy <dallacia.enrico@protonmail.com>
2023-12-08 12:06:13 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/charted_bot.rs
+++ src/charted_bot.rs
@@ -1,21 +1,23 @@
 use robotics_lib::{
-    energy::Energy,
     interface::{discover_tiles, Direction},
     runner::Runnable,
-    world::World,
+    utils::LibError,
+    world::{coordinates::Coordinate, World},
 };
 
 use crate::charted_coordinate::ChartedCoordinate;
 
 pub struct ChartedBot {
-    energy: Energy,
+    width: u32,
+    length: u32,
     coordinates: ChartedCoordinate,
 }
 
 impl ChartedBot {
-    pub fn new(energy: Energy, coordinates: ChartedCoordinate) -> Self {
+    pub fn new(length: u32, coordinates: ChartedCoordinate) -> Self {
         ChartedBot {
-            energy,
+            width: 3,
+            length,
             coordinates,
         }
     }
@@ -24,9 +26,39 @@ impl ChartedBot {
         &mut self,
         robot: &mut impl Runnable,
         world: &mut World,
-        start_pos: ChartedCoordinate,
         dir: Direction,
-    ) {
-        //let res = discover_tiles(robot, world, &vec![(0usize, 1usize)]);
+    ) -> Result<(), LibError> {
+        //Calculates cost for the discovery
+
+        let to_visit = match dir {
+            Direction::Up => {}
+            Direction::Down => {
+                let x: usize;
+                let y: usize;
+                println!("col: {}", self.coordinates.get_col());
+                if self.coordinates.get_col() < 1 {
+                    return Err(LibError::OutOfBounds);
+                }
+                for x in self.coordinates.get_col() - 1..=self.coordinates.get_col() + 1 {
+                    for y in self.coordinates.get_row()
+                        ..self.coordinates.get_row() + self.length as usize
+                    {
+                        println!("Tile to discover: ({x},{y})");
+                    }
+                }
+            }
+            Direction::Left => todo!(),
+            Direction::Right => todo!(),
+        };
+
+        if robot
+            .get_energy()
+            .has_enough_energy((self.length * self.width * 3) as usize)
+        {
+            let res = discover_tiles(robot, world, &vec![(5, 5)]);
+            Ok(())
+        } else {
+            Err(LibError::NotEnoughEnergy)
+        }
     }
 }
