178d0142f3f6a4efd59e7c509c075183778320ee
Riccardo Libanora <riccardo.libanora@gmail.com>
2023-12-09 18:54:42 +0100

    made world useful

--- src/charted_world.rs
+++ src/charted_world.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
 
-use robotics_lib::interface::{robot_map, robot_view};
+use robotics_lib::interface::{discover_tiles, robot_map, robot_view};
 use robotics_lib::runner::Runnable;
+use robotics_lib::utils::LibError;
 use robotics_lib::world::tile::Tile;
 use robotics_lib::world::World;
 
@@ -20,16 +22,53 @@ impl ChartingTool for ChartedWorld {
 }
 
 impl ChartedWorld {
-    pub fn init(&mut self, world: &World, dimension: usize) {
-        self.map = robot_map(world).unwrap_or(vec![vec![None; dimension]; dimension]);
+    pub fn init(&mut self, world: &World) -> Result<(), &str> {
+        match robot_map(world) {
+            None => Err("This literally should not be able to happen..."),
+            Some(map) => {
+                self.map = map;
+                Ok(())
+            }
+        }
     }
 
-    pub fn at(&self, coordinate: ChartedCoordinate) -> Option<Tile>{
-        self.map[coordinate.get_row()][coordinate.get_col()].clone()
+    pub fn at(&self, coordinate: ChartedCoordinate) -> Option<Tile> {
+        self.map[coordinate.0][coordinate.1].clone()
     }
 
-    pub fn get(&self) -> &Vec<Vec<Option<Tile>>> {
+    pub fn get_map(&self) -> &Vec<Vec<Option<Tile>>> {
         &self.map
+    }
+
+    pub fn set(&mut self, tile: &Tile, coordinate: ChartedCoordinate) -> Result<(), Tile> {
+        match self.at(coordinate) {
+            None => {
+                self.map[coordinate.0][coordinate.1] = Some(tile.clone());
+                Ok(())
+            }
+            Some(old_tile) => Err(old_tile.clone()),
+        }
+    }
+
+    pub fn set_unchecked(&mut self, tile: &Tile, coordinate: ChartedCoordinate) {
+        self.map[coordinate.0][coordinate.1] = Some(tile.clone());
+    }
+
+    pub fn set_multiple(&mut self, to_change: &Vec<(&Tile, ChartedCoordinate)>) -> Result<(), (Tile, ChartedCoordinate)> {
+        for i in to_change {
+            match self.set(i.0, i.1) {
+                Ok(_) => {}
+                Err(tile) => { return Err((i.0.clone(), i.1)); }
+            }
+        }
+        Ok(())
+    }
+
+    // these names are getting out of hand...
+    pub fn set_multiple_unchecked(&mut self, to_change: &Vec<(&Tile, ChartedCoordinate)>) {
+        for i in to_change {
+            self.set_unchecked(i.0, i.1);
+        }
     }
 
     pub fn update(&mut self, world: &World, coordinates: &Vec<ChartedCoordinate>) {
@@ -38,8 +77,10 @@ impl ChartedWorld {
 
         let map = option.unwrap();
         for point in coordinates.iter() {
-            if let Some(tile) = map[point.get_row()][point.get_col()].as_ref() {
-                self.map[point.get_row()][point.get_col()] = Some(tile.clone());
+            if map[point.0][point.1].is_some() && self.map[point.0][point.1] != map[point.0][point.1] {
+                self.map[point.0][point.1] = map[point.0][point.1].clone();
+            } else if self.map[point.0][point.1].is_some() && map[point.0][point.1].is_none() {
+                self.map[point.0][point.1] = None;
             }
         }
     }
@@ -52,28 +93,26 @@ impl ChartedWorld {
         for i in 0..view.len() {
             for j in 0..view.len() {
                 if view[i][j].is_some() {
-                    self.map[i + conversion_coordinate.get_row()][j + conversion_coordinate.get_col()] = view[i][j].clone();
+                    self.map[i + conversion_coordinate.0][j + conversion_coordinate.1] = view[i][j].clone();
                 }
             }
         }
     }
 
-    // pub fn update_discover(&mut self, robot: &mut impl Runnable, world: &mut World, to_discover: &Vec<ChartedCoordinate>)
-    //                        -> Result<HashMap<(usize, usize), Option<(TileType, Content)>>, LibError> {
-    //     todo!();
-    //     return match discover_tiles(robot, world, &to_discover.iter().map(|c| (c.get_row(), c.get_col())).collect::<Vec<(usize, usize)>>()) {
-    //         Ok(hm) => {
-    //             todo!();
-    //             for ((x, y), tile) in hm.iter() {
-    //                 // self.map[*x][*y] = tile.clone();
-    //             }
-    //             Ok(hm)
-    //         }
-    //         Err(err) => {
-    //             Err(err)
-    //         }
-    //     }
-    // }
+    pub fn update_discover(&mut self, robot: &mut impl Runnable, world: &mut World, to_discover: &Vec<ChartedCoordinate>)
+                           -> Result<HashMap<(usize, usize), Option<Tile>>, LibError> {
+        return match discover_tiles(robot, world, &to_discover.iter().map(|c| (c.0, c.1)).collect::<Vec<(usize, usize)>>()) {
+            Ok(hm) => {
+                for ((x, y), tile) in hm.iter() {
+                    self.map[*x][*y] = tile.clone();
+                }
+                Ok(hm)
+            }
+            Err(err) => {
+                Err(err)
+            }
+        };
+    }
 
     pub fn update_all(&mut self, world: &World) {
         let map = robot_map(world);
@@ -81,8 +120,10 @@ impl ChartedWorld {
             let map = map.unwrap();
             for (i, row) in map.iter().enumerate() {
                 for (j, tile) in row.iter().enumerate() {
-                    if tile.is_some() {
+                    if tile.is_some() && self.map[i][j] != map[i][j] {
                         self.map[i][j] = map[i][j].clone();
+                    } else if self.map[i][j].is_some() && tile.is_none() {
+                        self.map[i][j] = None;
                     }
                 }
             }
