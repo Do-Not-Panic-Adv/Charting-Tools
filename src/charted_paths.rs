ee588c84e3a5597ac8df257823ccf2d031ddd354
matteobregola <matteobregola@gmail.com>
2023-12-09 16:23:44 +0100

    documentation and Direction

--- src/charted_paths.rs
+++ src/charted_paths.rs
@@ -7,53 +7,123 @@ use robotics_lib::interface::look_at_sky;
 use robotics_lib::utils::calculate_cost_go_with_environment;
 use robotics_lib::world::tile::{Content, Tile, TileType};
 use robotics_lib::world::World;
-
+use robotics_lib::interface::Direction;
 use crate::charted_coordinate::ChartedCoordinate;
 use crate::ChartingTool;
 
-/// -----Welcome to the Pathfinder!-----
-/// The idea behind the Pathfinder is to allow the user to better interact with the robot_map
+/// -----Welcome to the ChartedPaths!-----
+/// The idea behind the ChartedPaths is to allow the user to better interact with the robot_map
 /// that contains the tiles he has visited. In particular the purpose behind this structure is to
 /// help the robot evaluate what is the cost to go from a visited position to another one and to
-/// find what is the path to do so!
+/// find what is the path to do so! This tool finds some interesting functionalities if it's used
+/// with the charted_map, for example, if the AI is a Firefighter, it can use the charted_map to
+/// save only the tiles with Water and then use the charted_path to find which ones is reachable
+/// with fewer cost and what is the path to it.
+/// Nb: This tool is designed to be used "one shot" when needed,
+/// meaning that the the structure should be initialized in the process tick and. The reason behind
+/// this is that the discovered world and the costs change very quickly so keeping updated the graph
+/// would be not very efficient and not very useful.
 ///
-/// Let's analyze the public functions provided with the Pathfinder structure
+/// Let's analyze the fields of the struct:
+///     - graph: Graph<ChartedCoordinate, u32, Undirected>.
+///          represents the undirected graph
+///          the node contains a ChartedCoordinate (usize,usize) of the discovered tile
+///          the edge between two nodes is the cost (u32) that the robot should spend by going
+///          from the coordinate of node_1 to the coordinate of node_2
+///     - indexes: Vec<Vec<Option<NodeIndex>>>.
+///          it's a map nxn that contains the "conversion" of the map's coordinates to their
+///          references in the graph.
+///     - teleports_edges: HashMap<EdgeIndex, bool>.
+///          contains the references to the edges in which one of the node is a teleport. Can be
+///          used for further development or to easily changing the cost of teleport operations.
 ///
-/// 1) from_map(&Vec<Vec<Option<Tile>>>)-> PathFinder
-///     Robotic_lib provide a function called robot_map(..) that returns a matrix nxn in which
+/// NB: since this struct is part of the Charting tool, the "new" function of its trait
+///  implementation
+///  ///     Example:
+/// ```
+///
+///          use charting_tools::charted_paths::ChartedPaths;
+///          use charting_tools::ChartingTool;
+///          let mut charted_paths=ChartedPaths::new();
+///
+/// ```
+///
+/// Let's analyze the public functions provided with the ChartedPaths structure
+///
+/// 1) pub fn init(&mut self, &Vec<Vec<Option<Tile>>>, &World)
+///     Robotic_lib provides a function called robot_map(..) that returns a matrix nxn in which
 ///     are "stored" the discovered tiles (seen or walked over) of the robot while
 ///     the other ones are set to None.
 ///     The function takes this map as parameter and crate a weighted graph of tiles choosing only
 ///     the tiles that the robot can walk over. The weight is relative to the actual
-///     energy consumption.
+///     energy consumption. Since the cost is evaluated also with respect to the environmental
+///     conditions is necessary to pass the World & reference
 ///     The function also takes care of the teleport functionality.
-///
 ///     Example:
-///         let pathfinder=PathFinder::from_map(&robot_map);
+///
+///         fn process_tick(& mut self, world:&mut World){
+///             let mut charted_path = ChartedPaths::new();
+///             charted_path.init(&robot_map(world).unwrap(), world);
+///         }
 ///
 ///
-/// 2) shortest_path_cost(&self, (usize, usize), (usize, usize)) ->Option<u32>
-///     Takes as parameter two coordinates, "from" and "to".
+///
+/// 2) shortest_path_cost(&self, ChartedCoordinate, ChartedCoordinate) -> Option<u32>
+///     Takes as parameter two coordinates, "from" and "to" as ChartedCoordinates.
 ///     Evaluates the cost of the shortest path between two coordinates using
 ///     Dijkstra algorithm (Complexity: O((V+E) log V). If the coordinates are out of bounds
-///     or if there isn't a path between them it returns None
-///
+///     or if there isn't a path between them it returns None.
 ///     Example:
-///         let pathfinder=PathFinder::from_map(&robot_map);
-///         let path_cost:Option<u32> = pathfinder.shortest_path_cost((0,0), (0,4));
 ///
-/// 3) shortest_path_cost_a_star(&self, (usize, usize), (usize, usize)) ->Option<u32>
-///     Same as shortest_path_cost but inside it used the A* algorithm
+///         fn process_tick(& mut self, world:&mut World){
+///             let my_coordinate = ChartedCoordinate::from(self.get_coordinate());
+///             let destination = ChartedCoordinate(1, 2);
+///             let distance=cp.shortest_path_cost(my_coordinate,destination);
+///         }
 ///
-/// 4) shortest_path(&self, (usize,usize), (usize,usize))->Option<(usize,Vec<(usize,usize)>)>
+///
+///
+/// 3) shortest_path_cost_a_star(&self, ChartedCoordinate, ChartedCoordinate) -> Option<u32>
+///     Same as shortest_path_cost but inside it uses the A* algorithm
+///
+///
+///
+/// 4) pub fn shortest_path(&self, ChartedCoordinate, ChartedCoordinate) ->
+///         Option<(usize, Vec<ChartedCoordinate>)>
 ///    Takes as parameter two coordinates, "from" and "to".
 ///    Evaluates both the COST to go from the first coordinate to the second and the PATH using the
-///    A* algorithm If the coordinates are out of bounds or if there isn't a path between them
-///    it returns None otherwise it returns a Vector of tuples in which each tuple is a coordinate
-///    of the robot_map that can be used to move the robot from one coordinate to another with the
-///    best possible energy consumption
-
-
+///    A* algorithm. If the coordinates are out of bounds or if there isn't a path between them
+///    it returns None. The path is expressed as a Vector of tuples in which each tuple is a
+///    coordinate of the robot_map (equal to the world's one) that can be used to move the robot
+///    from one coordinate to another with the best possible energy consumption. NOTE: the path
+///    comprehend both the from  coordinate and the end coordinate (So if the robot wants to move
+///    to the objective tile it can skip to move to the first coordinate).
+///    Example:
+///
+///         fn process_tick(& mut self, world:&mut World){
+///             let my_coordinate = ChartedCoordinate::from(self.get_coordinate());
+///             let destination = ChartedCoordinate(1, 2);
+///             let best_path=cp.shortest_path(my_coordinate,destination);
+///             match best_path {
+///                None => { //path not found}
+///                 Some(path) => {//cost = path.0, coordinates=path.1
+///                     for i in 1..path.1.len(){ //movin
+///                         let updated_view= match go(self, world, Direction::Up){
+///                             Ok((view,_)) => {Some(view)}
+///                             Err(_) => {None}
+///                         };
+///                     }
+///                 }
+///             }
+///         }
+///
+///
+/// 5) pub fn coordinates_to_direction(ChartedCoordinate, ChartedCoordinate) -> Result<Direction, ()>
+///     This function converts what is the direction the robot need to move if he want to go from
+///     a coordinate to another one. For example if the robot is in (0,0) and he wants to move to
+///     (1,0) then he needs to pass Direction::Down to the go interface.
+///
+///
 pub struct ChartedPaths {
     pub graph: Graph<ChartedCoordinate, u32, Undirected>,
     pub indexes: Vec<Vec<Option<NodeIndex>>>,
@@ -71,26 +141,7 @@ impl ChartingTool for ChartedPaths {
 }
 
 #[allow(unused)]
-fn eval_weight(from: &ChartedCoordinate, to: &ChartedCoordinate, map: &Vec<Vec<Option<Tile>>>, world: &World) -> u32 {
-    if ChartedCoordinate::is_close_to(from, to) {
-        let env_cond = look_at_sky(world);
 
-        match (map[from.0][from.1].as_ref(), map[to.0][to.1].as_ref()) {
-            (Some(tile_from), Some(tile_to)) => {
-                let base_cost = tile_from.tile_type.properties().cost();
-                let base_cost = calculate_cost_go_with_environment(base_cost, env_cond, tile_from.tile_type) as u32;
-                if tile_from.elevation < tile_to.elevation {
-                    let elevation_cost = ((tile_from.elevation - tile_to.elevation) as i32).pow(2) as u32;
-                    return (base_cost + elevation_cost);
-                }
-                return base_cost;
-            }
-            _ => { return u32::MAX; }
-        }
-    } else {
-        u32::MAX
-    }
-}
 
 
 impl ChartedPaths {
@@ -124,7 +175,7 @@ impl ChartedPaths {
                                     // seen it.
                                     self.graph.add_edge(*present_tile,
                                                         *next_tile,
-                                                        eval_weight(&ChartedCoordinate(i, j), &ChartedCoordinate(i, j + 1), &robot_map, &world));
+                                                        ChartedPaths::eval_weight(&ChartedCoordinate(i, j), &ChartedCoordinate(i, j + 1), &robot_map, &world));
                                 }
                             }
                         }
@@ -139,7 +190,7 @@ impl ChartedPaths {
 
                                     self.graph.add_edge(*present_tile,
                                                         *next_tile,
-                                                        eval_weight(&ChartedCoordinate(i, j), &ChartedCoordinate(i + 1, j), &robot_map, &world));
+                                                        ChartedPaths::eval_weight(&ChartedCoordinate(i, j), &ChartedCoordinate(i + 1, j), &robot_map, &world));
                                 }
                             }
                         }
@@ -223,6 +274,25 @@ impl ChartedPaths {
         };
     }
 
+    pub fn coordinates_to_direction(from: ChartedCoordinate, to: ChartedCoordinate) -> Result<Direction, ()>{
+
+        if from.1 > to.1 {
+            return Ok(Direction::Left);
+        }
+        if from.1 <to.1{
+            return Ok(Direction::Right);
+        }
+        if from.0 > to.0 {
+            return Ok(Direction::Up);
+        }
+        if from.0 < to.0 {
+            return Ok(Direction::Down);
+        }
+
+        // err casa: i pass the same coordinate
+        return Err(());
+    }
+
     fn check_boundaries(&self, from: ChartedCoordinate, to: ChartedCoordinate) -> bool {
         if (from.0 >= self.indexes.len()) || (from.1 >= self.indexes.len()) ||
             (to.0 >= self.indexes.len()) || (to.1 >= self.indexes.len()) {
@@ -281,6 +351,26 @@ impl ChartedPaths {
                 }
             }
             indexes.push(row);
+        }
+    }
+    fn eval_weight(from: &ChartedCoordinate, to: &ChartedCoordinate, map: &Vec<Vec<Option<Tile>>>, world: &World) -> u32 {
+        if ChartedCoordinate::is_close_to(from, to) {
+            let env_cond = look_at_sky(world);
+
+            match (map[from.0][from.1].as_ref(), map[to.0][to.1].as_ref()) {
+                (Some(tile_from), Some(tile_to)) => {
+                    let base_cost = tile_from.tile_type.properties().cost();
+                    let base_cost = calculate_cost_go_with_environment(base_cost, env_cond, tile_from.tile_type) as u32;
+                    if tile_from.elevation < tile_to.elevation {
+                        let elevation_cost = ((tile_from.elevation - tile_to.elevation) as i32).pow(2) as u32;
+                        return (base_cost + elevation_cost);
+                    }
+                    return base_cost;
+                }
+                _ => { return u32::MAX; }
+            }
+        } else {
+            u32::MAX
         }
     }
 }
